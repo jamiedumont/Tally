d2553d37642e31ff32f69d99ebd057efb6055a9a
Jamie Dumont <jamie@jamiedumont.co.uk>
2021-10-13 09:51:26 +0100

    Initial Commit

---
+++ Shared/TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyApp.swift
+//  Shared
+//
+//  Created by Jamie Dumont on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
